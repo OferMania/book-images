6013fc21173741c1b84c0e2c78859f350725d534
Ofer Gill <oferhirsch.gill@gmail.com>
2023-11-08 18:03:07 -0700

    Cleanups and clarifications in the README

--- README.md
+++ README.md
@@ -1,11 +1,57 @@
 ### Getting started
 
-Install rust (and family):
+All instructions were tested on my Ubuntu 22.04.3 desktop. Your mileage may vary on other OS's. I might consider making a docker or containerd dev-env container to simplify setup, especially for this first section.
+
+Install rust (and family) if you don't have it already:
+```bash
 $ curl https://sh.rustup.rs -sSf | sh
+```
 
 Install tools that cargo needs:
+```bash
 $ sudo apt-get install gcc
 $ sudo apt install build-essential
-
+```
 Install pkg-config to find openSSL:
+```bash
 $ sudo apt install pkg-config libssl-dev
+```
+
+### Project id's and toml
+
+On this site (https://blockfrost.io/) add two new projects: One project for network Cardano Mainnet, and one project for network IPFS. You may give these projects any names you want.
+
+Next, Take careful note of the project ID's mentioned for these. Suppose they are mainnetXyz91Uvw8Rst7Pqr6Mno5Jkl4Ghi3Def2 and mainnetAbc1Def2Ghi3Jkl4Mno5Pqr6Stu7Vwx8 for the Cardano and IPFS projects respectively (no, these aren't my actual project ID's...).
+
+Make a file called .blockfrost.toml within this repo (it's among the .gitignore files). The contents of this file should look like:
+```toml
+project_id = "mainnetXyz91Uvw8Rst7Pqr6Mno5Jkl4Ghi3Def2"
+cardano_network = "https://cardano-mainnet.blockfrost.io/api/v0"
+ipfs_id = "mainnetAbc1Def2Ghi3Jkl4Mno5Pqr6Stu7Vwx8"
+ipfs_network = "https://ipfs.blockfrost.io/api/v0"
+```
+
+### Building and running
+
+Build the project via cargo:
+```bash
+$ cargo build
+```
+
+If you wish to list the valid policy id's, do:
+```bash
+$ ./target debug/book_images
+```
+
+KEEP IN MIND: A collection_id is a policy_id. So look at the output from previous step for a policy_id you wish to get images for.
+
+Suppose you decide you want to download images at relative directory "images" for policy id 6a1388037f4a58d3acd4c121a94a6ebb0ca428a53d4321ce1f7ac28d, then you do:
+```bash
+$ ./target debug/book_images 6a1388037f4a58d3acd4c121a94a6ebb0ca428a53d4321ce1f7ac28d -p images
+```
+
+### Under development
+
+- Downloading images (I need a valid project ID to test with for a network IPFS). I have the code written, just need to try it & see if it works.
+- Barely started: Outputting a json files into the user specified path to track download status of image files. The idea here is this: Each json file is in format `<policy-id>.json` and contains a list of image id's we're intending to download, and the status on if we finished downloading them or not. If this json file exists, we can skip trying to locate the 10 image hashes for the policy-id. Next, for the images in this file, we check if the file exists and its status was set to download completed. If yes, we skip, otherwise, we know we still need to download the file & possibly clobber the older copy if that download was incomplete.
+ 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use blockfrost::{load, BlockFrostApi};
+use blockfrost::{load, BlockFrostApi, IpfsApi, IpfsSettings};
 use clap::Parser;
 use error_chain::error_chain;
+use std::fs::{File, self};
+use std::io::Write;
+use std::path::Path;
 use std::{io::Read, collections::HashSet};
 use std::env;
 use serde::{Deserialize, Serialize};
@@ -12,10 +15,26 @@ fn build_api() -> blockfrost::Result<BlockFrostApi> {
     Ok(api)
 }
 
+fn build_ipfs() -> blockfrost::Result<IpfsApi> {
+    let configurations = load::configurations_from_env()?;
+    let project_id = configurations["ipfs_id"].as_str().unwrap();
+    let api = IpfsApi::new(project_id, IpfsSettings::new());
+    Ok(api)
+}
+
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
+    /// The policy ID to look for. Lists all policies if this is omitted.
     name: Option<String>,
+
+    /// The path to save the files in
+    #[arg(short, long, default_value_t = String::from("."))]
+    path: String,
+
+    /// The maximum number of images to download per policy ID
+    #[arg(short, long, default_value_t = 10)]
+    quota: u16,
 }
 
 error_chain! {
@@ -40,8 +59,22 @@ struct CollectionBody {
     data: Vec<CollectionEntry>,
 }
 
+#[derive(Serialize, Deserialize)]
+struct AssetFile {
+    #[serde(rename = "mediaType")]
+    media_type: String,
+    name: String,
+    src: String,
+}
+
+// #[derive(Serialize, Deserialize)]
+// struct ImageHashHistory {
+//     image_hash: String,
+//     status: String,
+// }
+
 fn main() -> Result<()> {
-    env::set_var("RUST_BACKTRACE", "1");
+    //env::set_var("RUST_BACKTRACE", "1");
     let cli = Cli::parse();
 
     let api = build_api().unwrap();
@@ -50,32 +83,36 @@ fn main() -> Result<()> {
     let mut body = String::new();
     res.read_to_string(&mut body)?;
 
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
-    println!("Body:\n{}", body);
-
     let status_u16 = res.status().as_u16();
+    if status_u16 < 200 || status_u16 >= 300 {
+        return Err(format!("Listing policy id's yielded status {}", status_u16).into());
+    }
+
+    if cli.name.is_none() {
+        // Without a policy_id, user is asking to list what's available, so show the body & exit with Ok
+        println!("Body:\n{}", body);
+        return Ok(());
+    }
+
+    // get the policy_id's
     let mut policy_ids : HashSet<String> = HashSet::new();
-    if status_u16 >= 200 && status_u16 < 300 {
-        let parsed: CollectionBody = serde_json::from_str(&body).unwrap();
-        for data_item in parsed.data {
-            policy_ids.insert(data_item.collection_id);
-        }
+    let parsed: CollectionBody = serde_json::from_str(&body).unwrap();
+    for data_item in parsed.data {
+        policy_ids.insert(data_item.collection_id);
     }
 
     println!("Policy Ids:\n{:#?}", policy_ids);
 
-    let mut policy_id = String::from("");
-    if cli.name.is_some() {
-        policy_id = cli.name.unwrap();
-        let exists = policy_ids.contains(&policy_id);
-        println!("Item {} is a policy_id: {}", policy_id, exists);
+    // does the specified policy exist?
+    let policy_id = cli.name.unwrap();
+    let exists = policy_ids.contains(&policy_id);
+    println!("Item {} is a policy_id: {}", policy_id, exists);
 
-        if !exists {
-            return Err(format!("policy_id not found: {}", policy_id).into());
-        }
+    if !exists {
+        return Err(format!("policy_id not found: {}", policy_id).into());
     }
 
+    // get the asset policies for the policy id
     let rt = tokio::runtime::Runtime::new().unwrap();
     let asset_policy = rt.block_on(api.assets_policy_by_id(&policy_id)).unwrap();
     println!("Asset Policy: {:#?}", asset_policy);
@@ -89,10 +126,66 @@ fn main() -> Result<()> {
 
     println!("Asset Policies to inspect: {:#?}", asset_policies);
 
+    // Loop below is SLOW, so show a notice so user doesn't think we froze or crashed
+    println!("Computing image sources...");
+
+    // get the IPFS src hashes for each asset_id
+    let mut image_srcs: HashSet<String> = HashSet::new();
     for asset_id in asset_policies {
         let asset = rt.block_on(api.assets_by_id(&asset_id)).unwrap();
-        println!("Asset Details: {:#?}", asset);
+ 
+        if asset.onchain_metadata.is_some() {
+            let asset_map = asset.onchain_metadata.unwrap();
+            if asset_map.contains_key("files") {
+                let asset_files : Vec<AssetFile> = serde_json::from_value(asset_map["files"].clone()).unwrap();
+                for asset_file in asset_files {
+                    // If the src starts with ipfs://, then trim it off, else just add it as-is
+                    let src_option = asset_file.src.strip_prefix("ipfs://");
+                    if src_option.is_some() {
+                        image_srcs.insert(src_option.unwrap().to_string());
+                    } else {
+                        image_srcs.insert(asset_file.src);
+                    }
+                }
+            }
+        }
     }
+
+    println!("Image srcs: {:#?}", image_srcs);
+
+    // TODO: Make json file to track list of image hashes & their download status. This file goes in the directory marked by cli.path
+    //
+    // let policy_path = Path::new(".policy");
+    // if !policy_path.exists() {
+    //     let create_result = fs::create_dir(policy_path);
+    //     if create_result.is_err() {
+    //         return Err(format!("unable to make path: {}", policy_path.as_os_str().to_str().unwrap()).into());
+    //     }
+    // }
+    // let policy_file = policy_id + &String::from(".json");
+
+
+    // TODO: Code to download & clobber incomplete png files from the list of hashes
+    //
+    // let ipfs_api = build_ipfs().unwrap();
+
+    // let path = Path::new(&cli.path);
+    // let png_extension = String::from(".png");
+
+    // for image_src in image_srcs {
+    //     let gateway = rt.block_on(ipfs_api.gateway(&image_src)).unwrap();
+    //     let fileName = image_src+&png_extension;
+    //     let filePath = path.join(fileName);
+    //     if (filePath.exists()) {
+    //         let rr = fs::remove_file(filePath);
+    //         // check rr
+    //     }
+    //     let mut file_s = File::create(filePath);
+    //     if (file_s.is_ok()) {
+    //         let mut file = file_s.unwrap();
+    //         file.write_all(&gateway);
+    //     }
+    // }
 
     Ok(())
 }
